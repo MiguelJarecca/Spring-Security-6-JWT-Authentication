7a59ba95965a4a50e437fd788003f18382e21d35
miguel <miguel.jarecca@gmail.com>
2023-11-21 13:16:54 -0500

    se registra usuario con su token generado

--- src/main/java/com/demo/configuration/SecurityConfig.java
+++ src/main/java/com/demo/configuration/SecurityConfig.java
@@ -2,10 +2,16 @@ package com.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.demo.jwt.JwtAuthFilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +19,12 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    AuthenticationProvider authProvider;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -27,7 +39,11 @@ public class SecurityConfig {
                     .requestMatchers("/auth/**").permitAll()
                     .anyRequest().authenticated()
                     )
-            .formLogin(withDefaults())
+            .sessionManagement(sessionManagement ->
+                sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authProvider)   
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)        
             .build();        
     }
 }
